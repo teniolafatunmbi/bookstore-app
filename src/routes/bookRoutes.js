127822b3e20c832055a8173c2a906b9124c666f0
Teniola <fatunmbiteniola001@gmail.com>
2021-05-24 14:12:55 +0100

    added authentication to the routes

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,15 +1,15 @@
 const express = require("express")
 const router = express.Router()
 const { createNewBook, fetchAllBooks, fetchSingleBook, updateSingleBook, deleteSingleBook } = require("../controllers/bookControllers");
-
+const { authenticateUser } = require("../middlewares/authentication")
 //POST request to /books to create a new book
 router.post("/books", createNewBook)
 
 //GET request to /books to fetch all books
-router.get("/books", fetchAllBooks)
+router.get("/books", authenticateUser, fetchAllBooks)
 
 //GET request to /books/id to fetch a single book
-router.get("/books/:id", fetchSingleBook)
+router.get("/books/:id", authenticateUser,fetchSingleBook)
 
 //PUT request to /books/id to update a single book
 router.put("/books/:id", updateSingleBook)
