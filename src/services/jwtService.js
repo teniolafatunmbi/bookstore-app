f5743476b920d4d4527c3f565df3664045c779fd
Teniola <fatunmbiteniola001@gmail.com>
2021-05-25 12:39:34 +0100

    added the services folder

---
+++ src/services/jwtService.js
@@ -0,0 +1,17 @@
+const jwt = require("jsonwebtoken");
+const secret = process.env.JWT_SECRET;
+const expiry = Number(process.env.TOKEN_EXPIRY);
+
+exports.createToken = (user) => {
+    try {
+        let token = jwt.sign({
+            id: user._id,
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName
+        }, secret, {expiresIn: expiry})
+    } catch (err) {
+        if(err) throw err;
+        return null;
+    }
+}
