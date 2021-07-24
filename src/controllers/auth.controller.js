6b06e1fecce3b5becd709cd1e6a295a0e08bb1d3
Teniola <fatunmbiteniola001@gmail.com>
2021-07-24 17:56:07 +0100

    refactored functions to use async/await

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,66 +2,69 @@ const User = require("../models/user")
 const bcrypt = require("bcryptjs")
 const { createToken } = require("../services/jwtService")
 
-exports.registerNewUser = (req, res) => {
+exports.registerNewUser = async(req, res) => {
     //fetch user details from req.body
     //check if a user with this username exists
-    User.findOne({username: req.body.username}, (err, existingUser) =>{
-        if(err) return res.status(500).json({ message: err })
-        if (existingUser) res.status(400).json({ message: "A user with this username already exists"})
-    })
-    //create a new user
-    User.create({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        username: req.body.username
-    }, (err, newUser) => {
-        if(err) res.status(500).json({ message: err })
-        //hash the user's password.
-        bcrypt.genSalt(10, (err, salt) => {
-            if(err) return res.status(500).json({ message: err })
-            bcrypt.hash(req.body.password, salt, (err, hashedPassword) => {
-                if(err) return res.status(500).json({ message: err })
-                //save password to database
-                newUser.password = hashedPassword;
-                newUser.save((err, savedUser) => {
-                    if(err) return res.status(500).json({ message: err })
+    try{
+        const existingUser = await User.findOne({username: req.body.username});
+        if (existingUser) res.status(400).json({ message: "A user with this username already exists"});
 
-                    
-                    //create JWT for user
-                    let token = createToken(savedUser);
-                    //send token to user
-                    if(!token) return res.status(500).json({ message: "Sorry, we could not authenticate you. Please login"})
-                    else return res.status(200).json({ message: "user registration successful", token})
-                    
-                }) 
-            })
-        })
+        //create a new user
+        const newUser = await User.create({
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    username: req.body.username
+                    });
+        const salt = await bcrypt.genSalt(10);
+        
+        //hash password
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        newUser.password = hashedPassword;
 
-    
-    })
-}
+        //save password to database
+        await newUser.save();
+        
+        //create JWT for user
+        let token = createToken(newUser);
 
-exports.loginUser = (req, res) => {
-    // Check if user exists.
-    User.findOne({username: req.body.username}, (err, foundUser) => {
-        if(err) return res.status(500).json({ message: err})
-        if(!foundUser) return res.status(401).json({ message: "Incorrect username"})
-        // else return res.status(200).json({ foundUser })
-    
-        // Compare user's password with stored hash.
+        //send token to user
+        if(!token) return res.status(500).json({ message: "Sorry, we could not authenticate you. Please login"});
+        else return res.status(200).json({ 
+            message: "user registration successful", token
+        });
+    }
+    catch(err){
+        throw err;
+    }
+};
+
+exports.loginUser = async(req, res) => {
+    try{
+        const foundUser = User.findOne({username: req.body.username});
+        if(!foundUser) return res.status(401).json({ 
+            message: "Incorrect username "
+        });
+
+        //compare user's password with stored hash
         let match = bcrypt.compare(req.body.password, foundUser.password)
-        if (!match) return res.status(401).json({ message: "Incorrect password"})
+        if (!match) return res.status(401).json({ 
+            message: "Incorrect password"
+        })
 
         //create JWT for user
         console.log(foundUser)
         let token = createToken(foundUser);
 
         //send token to user
-        if(!token) return res.status(500).json({ message: "Sorry, we could not authenticate you. Please login"})
+        if(!token) return res.status(500).json({ 
+            message: "Sorry, we could not authenticate you. Please login"
+        })
         else return res.status(200).json({ 
             message: "user logged in",
             token
-        }) 
-        
-        })
+        });
+    }
+    catch(err){
+        throw err;
+    }
 }
