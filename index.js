f4e8b8b236a9b462a709eb0fcc7e0c7d0c10dbb2
Teniola <fatunmbiteniola001@gmail.com>
2021-07-24 16:06:31 +0100

    renamed required routes

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const express = require("express");
-const dbSetup = require("./src/database/setup")
+const dbSetup = require("./src/database/setup");
 const app = express();
 require("dotenv").config();
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4000;
 
 //REQUIRE ROUTES
-const bookRoutes = require("./src/routes/bookRoutes")
-const authRoutes = require("./src/routes/authRoutes")
+const bookRoutes = require("./src/routes/book.routes");
+const authRoutes = require("./src/routes/auth.routes");
 
 // SEEDERS
-const { seedAdmin } = require("./src/seeders/admin")
+const { seedAdmin } = require("./src/seeders/admin");
 // console.log(seedAdmin());
 
-app.use(express.json())
+app.use(express.json());
 
 dbSetup();
 
-app.use("/auth", authRoutes)
-app.use(bookRoutes)
+app.use("/auth", authRoutes);
+app.use(bookRoutes);
 
-app.listen(port, (err) =>{
-    if(err) console.log(err)
-    else console.log(`Server is running on port ${port}`)
+app.listen(port, (err) => {
+    if (err) console.log(err);
+    else console.log(`Server is running on port ${port}`);
 })
