3bba7b83f0d2b4f1b74a171216067eaa1f08124e
Teniola <fatunmbiteniola001@gmail.com>
2021-07-26 17:30:43 +0100

    renamed file from setup to config

--- src/config/index.js
+++ src/config/index.js
@@ -3,12 +3,12 @@ require('dotenv').config();
 const connectionString = process.env.DB_URL;
 
 // connect to database
-module.exports = function (){
+module.exports = dbSetup = () => {
     mongoose.connect((connectionString), {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }, (err) => {
     if(err) console.log(err);
-    else console.log("database connected successfully")
+    else console.log("database connected successfully");
 })
 }
