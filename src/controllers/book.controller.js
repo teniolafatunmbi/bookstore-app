c95c655ce38b51fc1ce37786ed772823c7cc96cf
Teniola <fatunmbiteniola001@gmail.com>
2021-07-26 16:48:30 +0100

    refactored the code to use async/await

--- src/controllers/book.controller.js
+++ src/controllers/book.controller.js
@@ -17,48 +17,48 @@ exports.createNewBook = function (req, res) {
     })
 }
 
-exports.fetchAllBooks = function (req, res) {
-    let conditions = {}
-    if(req.query.category){
-        conditions.category = req.query.category
+exports.fetchAllBooks = async(req, res) => {
+    try{
+        let conditions = {}
+        if(req.query.category){
+            conditions.category = req.query.category
+        }
+        //check req.query for filters
+        console.log(conditions)
+        // console.log(req.query)
+        //if filters exist, use them in Model.find query
+        console.log(Book.find({}))
+        const books = await Book.find(req.query);
+        return res.status(200).json({ books });
     }
-    //check req.query for filters
-    console.log(conditions)
-    // console.log(req.query)
-    //if filters exist, use them in Model.find query
-    console.log(Book.find({}))
-    Book.find(req.query, (err, books) => {
-        if(err) return res.status(500).json({ message: err })
-        else{
-            return res.status(200).json({ books })
-        } 
-    
-    })
+    catch(err){
+        throw err;
+    }
 }  
 
 
-exports.fetchSingleBook = function (req, res) {
-    Book.findOne({ _id: req.params.id}, (err, book) => {
-        if (err){ 
-            return res.status(500).json({ message: err})
-        }
-        else if (!book){
-            return res.status(404).json({ message: "book not found" })
-        } 
-        else{
-            return res.status(200).json({ book })
-        }
-    })  
+exports.fetchSingleBook = async(req, res) => {
+    try{
+        const book = await Book.findOne({ _id: req.params.id});
+        if(!book) return res.status(404).json({ message: "book not found"});
+        else return res.status(200).json({ book });
+    }
+    catch(err){
+        throw err;
+    }
 }
 
-exports.updateSingleBook = function (req, res) {
-    Book.updateOne({ category: req.body.category, imageUrl: req.body.imageUrl, tags: req.body.tags }, (err, book) => {
-        if(err) return res.status(500).json({ message: err})
-        else return res.status(200).json({ message: `Changed info of student with id: ${req.params.id}`, book })
-    })
+exports.updateSingleBook = async(req, res) => {
+    try{
+        const book = await Book.updateOne({ category: req.body.category, imageUrl: req.body.imageUrl, tags: req.body.tags });
+        return res.status(200).json({ message: `Changed info of student with id: ${req.params.id}`, book });
+    }
+    catch(err){
+        throw err;
+    }
 }
 
-exports.deleteSingleBook = function (req, res) {
+exports.deleteSingleBook = (req, res) => {
     Book.deleteOne({ _id: req.params.id }, (err) => {
         if(err) return res.status(500).json({ message: err });
         else return res.status(200).json({ message: `Student with id: ${req.params.id} deleted`})
