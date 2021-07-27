563fcafeacfe6b4a00caf2f5d74146f13914a45c
Teniola <fatunmbiteniola001@gmail.com>
2021-07-27 22:59:02 +0100

    % bug fixes %

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -2,23 +2,26 @@ const { decodeToken } = require("../services/jwtService");
 
 exports.authenticateUser = (req, res, next) => {
     // check if there is an authorization token
-    if(!req.headers.authorization) return res.status(401).json({ message: "Authorization header required"})
-    next()
+    if(!req.headers.authorization) return res.status(401).json({ message: "Authorization header required"});
     let splittedHeader = req.headers.authorization.split(" ");
-    // console.log(splittedHeader)
-    if(splittedHeader[0] !== "Bearer") return res.status(401).json({ message: "Authorization format is Bearer <token>" })
+    
+    if(splittedHeader[0] !== "Bearer") return res.status(401).json({ message: "Authorization format is Bearer <token>" });
 
     let token = splittedHeader[1];
     
     // decode the token
-   let decodedToken = decodeToken(token);
-   // check if valid
+    let decodedToken = decodeToken(token);
+
+    // check if valid
     if(!decodedToken) return res.status(401).json({ message: "invalid authorization token. please login"})
-    else req.user = decodedToken;
+    else {
+        req.user = decodedToken;
+        next();
+    }
+    
 }
 
 exports.checkIfAdmin = (req, res, next) => {
-    console.log(req.user)
     if(req.user.role !== "admin") return res.status(401).json({ message: "this route is restricted to admin users"});
     return next();
 }
